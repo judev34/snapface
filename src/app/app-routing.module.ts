3a88bfd4f6107aba874696411dd4e871faacb3ca
SolidJulien <31781936+judev34@users.noreply.github.com>
2022-08-12 14:27:26 +0200

    feat(FaceSnaps): mise en place router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'facesnaps', component: FaceSnapListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
